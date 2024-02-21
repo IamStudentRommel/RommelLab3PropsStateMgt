60e0f8fbbf93de0a4f9068176ddc99dc59fad4b5
IamStudentRommel <rommel.hipos@edu.sait.ca>
2024-02-21 16:08:42 -0700

    init

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+import ToDoList from './component/ToDoList';
+
+function App() {
+  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
+
+  return <ToDoList tasks={tasks} />;
+}
+
+export default App;
